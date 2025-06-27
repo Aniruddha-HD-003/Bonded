e464fd778cea9ad79df66c7c21edcc6a5e3564eb
Aniruddha H D <154432249+Aniruddha-HD-003@users.noreply.github.com>
2025-06-27 20:06:25 +0530

    Update settings_production.py

--- backend/bonded_backend/settings_production.py
+++ backend/bonded_backend/settings_production.py
@@ -5,6 +5,7 @@ Production settings for bonded_backend project.
 import os
 from pathlib import Path
 from .settings import *
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
